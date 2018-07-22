6a2ee7076f9225ec68fc9a70a740c5a3d0b818fc
unknown <osvaldo_cavalcanti_filho@hotmail.com>
2018-07-22 03:26:58 +0100

    logic-login_ok

--- Apps4Society/src/apps4Society/dao/User_control.java
+++ Apps4Society/src/apps4Society/dao/User_control.java
@@ -16,6 +16,12 @@ import Interf.DataUser;
 
 public class User_control implements DataUser{
 	
+	protected final static String cript_M ="MD5";
+	protected final static String format_String_hex="%02X";
+	protected final static String COLUNA_LOGIN ="login";
+	private   final static String COLUNA_PASS = "pass";
+	private final static String COLUNA_COD = "codigo";
+	
 	
 
 	public void CadaddUser(User user) throws ClassNotFoundException, SQLException, UnsupportedEncodingException, NoSuchAlgorithmException {
@@ -29,11 +35,11 @@ public class User_control implements DataUser{
 		
 		String pass_user = user.getPass();
 		
-		final MessageDigest md = MessageDigest.getInstance("MD5");
+		final MessageDigest md = MessageDigest.getInstance(cript_M);
 		final byte[] pass_hash = md.digest(pass_user.getBytes("UTF-8"));
 		StringBuilder hexString = new StringBuilder();
 		for(byte bb : pass_hash) {
-			hexString.append(String.format("%02X", 0xFF & bb));
+			hexString.append(String.format(format_String_hex, 0xFF & bb));
 		}
 		String senhacript = hexString.toString();
 		user.setPass(senhacript);
@@ -98,6 +104,7 @@ public class User_control implements DataUser{
 			if(user.getLogin().equals(log) && user.getPass().equals(pass) && user.getCodigo().equals(codigo)) {
 				return true;
 			}
+				
 		}
 		cx.close();
 		rx.close();
@@ -106,14 +113,23 @@ public class User_control implements DataUser{
 	}
 
 	public boolean efetuarLogin(String login, String pass, String codigo) throws ClassNotFoundException, SQLException {
-		
+		/*
+		 * Codigo que faz a verificao se o usuario pode logar ou nao
+		 * usando uma "autenticacao de dois fatores"
+		 * com a senha criptografada e necessario verificar se a senha do usuario [e igual a mesma "hash"
+		 * depois e verificado se o codigo que ele inseriu quando se cadastrou [e o codigo gravado
+		 * pelo administrador do sistema.
+		 */
 		try{
 			String cod_p = "49946325";
 			String pass_reali =creatCriptLoginVerific(pass);
+			String login_query = "\'"+login+"\'";
+			String pass_query = "\'"+pass_reali+"\'";
 			
 			Connection cx = ConfBanco.getConnection();
 			
-			String sql = "SELECT login,pass,codigo FROM usuarios";
+			String sql = "SELECT * FROM usuarios WHERE login="+login_query + " AND pass="+pass_query;
+			System.err.println(sql);
 		
 			PreparedStatement st = (PreparedStatement)cx.prepareStatement(sql);
 			ResultSet rx = st.executeQuery();
@@ -125,12 +141,12 @@ public class User_control implements DataUser{
 				String pas = rx.getString("pass");
 				String cod = rx.getString("codigo");
 				
-				
-				if(log.equals(login) && pas.equals(pass_reali) && cod_p.equals(cod)){
+				if((codigo.equals(cod_p)) && (cod_p.equals(cod)) ){
 					condxStop = true;
 					return true;
 					
 				}
+				
 				if(condxStop) {
 					break;
 				}
@@ -139,6 +155,8 @@ public class User_control implements DataUser{
 			}
 			
 		}catch(Exception e) {
+			e.printStackTrace();
+			
 			
 		}
 		
@@ -148,11 +166,11 @@ public class User_control implements DataUser{
 	@Override
 	public String creatCriptLoginVerific(String pass) throws NoSuchAlgorithmException, UnsupportedEncodingException {
 		// TODO Auto-generated method stub
-		final MessageDigest md = MessageDigest.getInstance("MD5");
+		final MessageDigest md = MessageDigest.getInstance(cript_M);
 		final byte[] pass_hash_login = md.digest(pass.getBytes("UTF-8"));
 		StringBuilder hexString_log = new StringBuilder();
 		for(byte bb : pass_hash_login) {
-			hexString_log.append(String.format("%02X", 0xFF & bb));
+			hexString_log.append(String.format(format_String_hex, 0xFF & bb));
 		}
 		String senhacript_login = hexString_log.toString();
 	
