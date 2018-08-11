8cb4d32087380a1b741134a49ffb8cdea49a0489
unknown <osvaldo_cavalcanti_filho@hotmail.com>
2018-08-11 07:58:42 +0100

    newbd

--- Apps4Society/src/apps4Society/conf/ConfBanco.java
+++ Apps4Society/src/apps4Society/conf/ConfBanco.java
@@ -5,28 +5,20 @@ public class ConfBanco {
 	
 	
 	public static Connection getConnection() throws SQLException, ClassNotFoundException{
-
-		String url = "jdbc:postgresql://192.168.31.143:5432/apps4Society";
-		String acc = "postgres";
-		String pass = "6036236";
-		Class.forName("org.postgresql.Driver");
 		
-		// [e necessario criar uma conexao SSL - com o banco de dados externo
-		// arquivos de configuracoes do post-
-		// cd data - nano postgresql.conf e nano pg_hba.conf
-		// criterios para o SSL alterar os arquivos de configuracoes a cima
-		// utilizar o comando opensSL para gerar um certificado e validar
-		// alguns arquivos vao ser necessarios tb armazenando a key
-		
-		// Atualmente . esta sem seguranca SSL entre cliente e servidor (BD-post)
+		String hostname="apps4society-2018.database.windows.net";
+		String appDB = "apps4Society";
+		String user = "rootosvaldo@apps4society-2018";
+		String password = "7154819osvaldo-";
+		String url = String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.database.windows.net;loginTimeout=50;",hostname,appDB,user,password);
 		
 	
-		
-		
-		Connection com = DriverManager.getConnection(url,acc,pass);
+		Connection com = DriverManager.getConnection(url);
 		
 		
 		return com;
 	}
+	
+	
 
 }
